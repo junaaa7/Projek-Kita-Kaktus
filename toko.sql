85a9ad5bfc71b75438eda1bbe532a5d8d28c94f7
Crafty-9 <mhmmdali2811@gmail.com>
2025-11-21 14:18:02 +0700

    Mengubah database

--- toko.sql
+++ toko.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 12, 2021 at 11:21 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.10
+-- Generation Time: Nov 21, 2025 at 08:16 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,7 +31,7 @@ CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `admin`
@@ -39,6 +39,34 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`id`, `username`, `password`) VALUES
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `id` int(11) NOT NULL,
+  `id_produk` int(11) DEFAULT NULL,
+  `nama_produk` varchar(255) NOT NULL,
+  `nama_pemesan` varchar(100) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `catatan` text DEFAULT NULL,
+  `jumlah` int(11) NOT NULL DEFAULT 1,
+  `harga_satuan` int(11) NOT NULL DEFAULT 0,
+  `total_harga` int(11) NOT NULL DEFAULT 0,
+  `tanggal` date NOT NULL,
+  `status` enum('baru','diproses','selesai','batal') NOT NULL DEFAULT 'baru'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pesanan`
+--
+
+INSERT INTO `pesanan` (`id`, `id_produk`, `nama_produk`, `nama_pemesan`, `email`, `alamat`, `catatan`, `jumlah`, `harga_satuan`, `total_harga`, `tanggal`, `status`) VALUES
+(1, 10, 'test', 'asdaad', 'asd@gmail.com', 'asd', '1', 1, 5000, 5000, '2025-11-18', 'batal');
 
 -- --------------------------------------------------------
 
@@ -52,15 +80,17 @@ CREATE TABLE `produk` (
   `nama` varchar(255) NOT NULL,
   `harga` varchar(100) NOT NULL,
   `wa` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `produk`
 --
 
 INSERT INTO `produk` (`id`, `gambar`, `nama`, `harga`, `wa`) VALUES
-(8, '962-kaktus (1).jpg', '1', '5000', '+6212752132'),
-(9, '932-kaktus (2).jpg', '2', '5000', '+6212752132');
+(8, '962-kaktus (1).jpg', 'Kaktus', '5000', '+6212752132'),
+(9, '932-kaktus (2).jpg', '2', '5000', '+6212752132'),
+(10, '590-logo 1.png', 'test', '5000', '+62');
+
 --
 -- Indexes for dumped tables
 --
@@ -70,6 +100,13 @@ INSERT INTO `produk` (`id`, `gambar`, `nama`, `harga`, `wa`) VALUES
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_produk` (`id_produk`);
 
 --
 -- Indexes for table `produk`
@@ -85,13 +122,29 @@ ALTER TABLE `produk`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD CONSTRAINT `pesanan_ibfk_1` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
